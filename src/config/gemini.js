178d18e5a5e860bd5c38a3cab41e4adb110e8e2c
Sresta Bhattarai <srestabhattarai@Mac-mini.local>
2025-03-25 16:31:46 +0545

    first commit

---
+++ src/config/gemini.js
@@ -0,0 +1,55 @@
+import {
+    GoogleGenerativeAI,
+    HarmCategory,
+    HarmBlockThreshold,
+  } from "@google/generative-ai";
+
+  import fs from "node:fs";
+  import mime from "mime-types";
+  
+  const apiKey = 'AIzaSyBFqDRhFlVQufA2H9jN6vkO9Q2xTByRZiE';
+  const genAI = new GoogleGenerativeAI(apiKey);
+  
+  const model = genAI.getGenerativeModel({
+    model: "gemini-2.0-flash",
+  });
+  
+  const generationConfig = {
+    temperature: 1,
+    topP: 0.95,
+    topK: 40,
+    maxOutputTokens: 8192,
+    responseModalities: [
+    ],
+    responseMimeType: "text/plain",
+  };
+  
+  async function run(prompt) {
+    const chatSession = model.startChat({
+      generationConfig,
+      history: [
+      ],
+    });
+  
+    const result = await chatSession.sendMessage(prompt);
+    // TODO: Following code needs to be updated for client-side apps.
+    const candidates = result.response.candidates;
+    for(let candidate_index = 0; candidate_index < candidates.length; candidate_index++) {
+      for(let part_index = 0; part_index < candidates[candidate_index].content.parts.length; part_index++) {
+        const part = candidates[candidate_index].content.parts[part_index];
+        if(part.inlineData) {
+          try {
+            const filename = `output_${candidate_index}_${part_index}.${mime.extension(part.inlineData.mimeType)}`;
+            fs.writeFileSync(filename, Buffer.from(part.inlineData.data, 'base64'));
+            console.log(`Output written to: ${filename}`);
+          } catch (err) {
+            console.error(err);
+          }
+        }
+      }
+    }
+    console.log(result.response.text());
+    return result.response.text();
+  }
+  
+  export default run;
